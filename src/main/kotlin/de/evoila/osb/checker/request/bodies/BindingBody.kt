bb7f9cff9362d3d40714b78e184d54eaf742ed0c
Johannes Strauß <jstrauss@evoila.de>
2018-12-18 08:54:20 +0100

    fix(bindingBody): do not set parameter if null

--- src/main/kotlin/de/evoila/osb/checker/request/bodies/BindingBody.kt
+++ src/main/kotlin/de/evoila/osb/checker/request/bodies/BindingBody.kt
@@ -1,10 +1,13 @@
 package de.evoila.osb.checker.request.bodies
 
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL
 import java.util.*
 
 abstract class BindingBody(
     val service_id: String?,
     val plan_id: String?,
+    @JsonInclude(NON_NULL)
     var parameters: Map<String, Any>? = null
 ) : RequestBody {
 
